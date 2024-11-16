2e1f463bd5f8df7de47d68e60d4369c67a95224d
HemaDeviU <uhemadevi1927@gmail.com>
2024-11-16 13:15:21 +0700

    contracts

---
+++ smart-contracts/src/Core.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+// mapping tokenId -> creator
+//create podcast episode -> get back id
+//get episodes of creator
+
+//mint nft of episode
+
+//sell nft
+
+//buy episode
+
+//podcast access
+//check access
+
+// 
+
+
+create nft for user take(metadat) 
+funritio ()
